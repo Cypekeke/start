d74ac0bc4ea2df3462e98f12b656f84bb94e64c1
Cypekeke <cyprian.mill@gmail.com>
2024-10-21 21:20:16 +0200

    zadanie

--- Test/src/Start.java
+++ Test/src/Start.java
@@ -1,9 +1,35 @@
+import java.util.Scanner;
 public class Start {
-    public static void main(String[] args) {
-        int a=2;
-        int b=3;
-        int c=a*b;
-        System.out.println(c);
+    public static void Age(String[] args) {
+        Scanner podajAge = new Scanner(System.in);//tworze obiekt podaj age
+
+        int age = podajAge.nextInt();                   //nextInt odczytuje age
+        System.out.println("Dzień dobry Piotrusia, słyszałem, że masz " + age + " lat i uczysz się programowania czy to prawda?");
+
+        if (IsAgeRight(age)) {                          //sprawdza czy wiek czy age jest zgodny z zalozeniami z boolean
+            System.out.println("Tak");
+        } else {
+            System.out.println("Nie, mam 30 i ucze się programowania.");
+        }
+    }
+
+    public static boolean IsAgeRight(int age)
+
+    {
+        return age == 30;
+    }
+
+    void Initials(String[] args) {
+        System.out.println("Podaj inicjały: ");
+        Scanner podajInitial = new Scanner(System.in);          //tworze obiekt podaj inicjalow (z racji ze z char mialbym wiecej pisania uzyje string)
+        String initial = podajInitial.next();
+        if (initial.equals("PS")) {
+            System.out.println("Tak");
+        } else {
+            System.out.println("Nie, moje inicjały to PS.");
+        }
+
 
     }
 }
+
